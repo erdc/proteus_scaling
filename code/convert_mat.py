579d761386132a1c5685e971226223e675061845
Aron Ahmadia <aron@ahmadia.net>
2015-03-12 15:13:46 -0500

    prep for 3D scaling work

--- code/convert_mat.py
+++ code/convert_mat.py
@@ -8,26 +8,36 @@ petsc4py.init(sys.argv)
 
 from petsc4py import PETSc
 
-def convert_mat(data_file, name):
+def convert_mat(data_file, name, block_size=3):
     viewer = PETSc.Viewer().createBinary(data_file, 'r')
-    A, viewer = load_A_blocked(viewer)
+    A, viewer = load_A_blocked(viewer, block_size)
     b = A.getVecLeft()
     b.load(viewer)
 
-    out_viewer = PETSc.Viewer().createBinary(data_file + '_blocked_3', 'w')
+    out_viewer = PETSc.Viewer().createBinary(data_file + '_blocked_' + str(block_size), 'w')
     A.view(out_viewer)
     b.view(out_viewer)
 
-def load_A_blocked(viewer):
+def load_A_blocked(viewer, block_size=3):
     A = PETSc.Mat()
     A.create()
-    A.setBlockSize(3)
+    A.setBlockSize(block_size)
     A.setFromOptions()
     A.load(viewer)
     return A, viewer
 
-name_prefix = 'plunging_breakers_twp_navier_stokes_p_L_'
-file_prefix = '/home/aron/adwr-nearshore-hydro/input/plunging-breakers/plunging_breakers_twp_navier_stokes_p_L_'
+def iter_systems(file_list):
+    for filename in file_list:
+        if os.path.exists(filename):
+            name = os.path.basename(filename)
+            yield filename, name
+
+block_size = int(sys.argv[1])
+
+mat_files = [filename for filename in sys.argv[2:] if not (filename.endswith('.info') or filename.endswith('.options'))]
+
+for file, name in iter_systems(mat_files):
+    convert_mat(file, name)
 
 def iter_systems(file_prefix, name_prefix):
     for i in count():
@@ -38,5 +48,5 @@ def iter_systems(file_prefix, name_prefix):
             return
 
 for file, name in iter_systems(file_prefix, name_prefix):
-    convert_mat(file, name)
+    convert_mat(file, name, block_size)
 
